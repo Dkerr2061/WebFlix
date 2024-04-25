aab223ff8efab3037de695c58a8eb385d59fe675
David Kerr <kerrky@gmail.com>
2024-04-25 01:06:41 -0400

    Finished views for the Cart Items

--- server/app.py
+++ server/app.py
@@ -206,8 +206,52 @@ class ReviewByID(Resource):
 
 api.add_resource(ReviewByID, '/reviews/<int:id>')
 
+class AllCartItems(Resource):
 
+    def get(self):
+        cart_items = CartItem.query.all()
+        body = [cart_item.to_dict(only=('id', 'movie_id', 'user_id')) for cart_item in cart_items]
+        return make_response(body, 200)
+    
+    def post(self):
+        try:
+            new_cart_item = CartItem(movie_id=request.json.get('movie_id'), user_id=request.json.get('user_id'))
+            db.session.add(new_cart_item)
+            db.session.commit()
+            body = new_cart_item.to_dict(only=('id', 'movie_id', 'user_id'))
+            return make_response(body, 201)
+        except:
+            body = {"error": "Could not add item to cart."}
+            return make_response(body, 400)
 
+api.add_resource(AllCartItems, '/cart_items')
+
+class CartItemsByID(Resource):
+
+    def get(self, id):
+        cart_item = db.session.get(CartItem, id)
+        if cart_item:
+            try:
+                body = cart_item.to_dict(only=('id', 'movie_id', 'user_id'))
+                return make_response(body, 200)
+            except:
+                body = {"error": "Could not process request."}
+                return make_response(body, 400)
+        else:
+            body = {"error": f"Cart item {id} could not be found."}
+            return make_response(body, 404)
+        
+    def delete(self, id):
+        cart_item = db.session.get(CartItem, id)
+        if cart_item:
+            db.session.delete(cart_item)
+            db.session.commit()
+            body = {}
+            return make_response(body, 204)
+        else:
+            body = {"error": f"Cart item {id} not found."}
+
+api.add_resource(CartItemsByID, '/cart_items/<int:id>')
 
 
 if __name__ == '__main__':
