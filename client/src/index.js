8e26b5aa837d9ee52936ad17efb7b558357c433f
David Kerr <kerrky@gmail.com>
2024-04-25 11:28:48 -0400

    Created an error page

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MovieList from "./components/MovieList";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,5 @@
+function ErrorPage() {
+  return <h1>Oops, this page does not exist.</h1>;
+}
+
+export default ErrorPage;
