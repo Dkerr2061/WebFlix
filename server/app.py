b007c62fab36a6c6d8bc4670746c1e5ec00e40ce
David Kerr <kerrky@gmail.com>
2024-04-24 23:25:44 -0400

    Added user views and added GET, POST and DELETE to User

--- server/app.py
+++ server/app.py
@@ -13,6 +13,7 @@ from models import Movie, User, CartItem, Review
 
 
 # Views go here!
+# ===================================================================================
 
 class AllMovies(Resource):
 
@@ -79,6 +80,71 @@ class MovieByID(Resource):
             return make_response(body, 404)
 
 api.add_resource(MovieByID, '/movies/<int:id>')
+
+# =================================================================================
+
+class AllUsers(Resource):
+
+    def get(self):
+        users = User.query.all()
+        body = [user.to_dict(only=('id', 'username', 'password_hash', 'type')) for user in users]
+        return make_response(body, 200)
+    
+    def post(self):
+        try:
+            new_user = User(username=request.json.get('username'), password_hash=request.json.get('password_hash'), type=request.json.get('type'))
+            db.session.add(new_user)
+            db.session.commit()
+            body = new_user.to_dict(only=('id', 'username', 'password_hash', 'type'))
+            return make_response(body, 201)
+        except:
+            body = {"error": "Could not create new user."}
+            return make_response(body, 400)
+
+api.add_resource(AllUsers, '/users')
+
+class UserByID(Resource):
+
+    def get(self, id):
+        user = db.session.get(User, id)
+        if user:
+            try:
+                body = user.to_dict(rules=('-reviews.movie', '-reviews.user', '-cart_items.movie_cart', '-cart_items.user_cart'))
+
+                body['movies'] = [movie.to_dict(only=('id', 'name', 'image', 'year', 'director', 'description', 'price')) for movie in user.movies]
+
+                return make_response(body, 200)
+            except:
+                body = {"error": "User not found"}
+                return make_response(body, 404)
+        else:
+            body = {"error": f"User {id} was not found."}
+            return make_response(body, 404)
+        
+    def delete(self, id):
+        user = db.session.get(User, id)
+        if user:
+            try:
+                db.session.delete(user)
+                db.session.commit()
+                body = {}
+                return make_response(body, 204)
+            except:
+                body = {"error": f"User {id} could not be deleted"}
+                return make_response(body, 400)
+        else:
+            body = {"error": f"User {id} could not be found"}
+            return make_response(body, 404)
+
+
+api.add_resource(UserByID, '/users/<int:id>')
+
+# =================================================================================
+
+
+
+
+
 
 
 if __name__ == '__main__':
