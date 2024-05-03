6631b7393670d449f74b5c1d2f39198311cdbf60
David Kerr <kerrky@gmail.com>
2024-05-02 20:09:32 -0400

    Updated the cart styling

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -8,40 +8,66 @@ function Cart({ cartItem, deleteCartItems }) {
   }
 
   return (
-    <div className="rounded-3xl border-2 border-slate-700 p-4 lg:p-8 flex-nowrap mb-4 max-lg:max-w-lg max-lg:mx-auto gap-y-4 glass ">
-      <Zoom>
-        <div className="flex items-center justify-center w-full mt-2">
-          <h2 className="italic font-bold font-serif text-4xl leading-9 text-slate-700 mx-4">
+    <ul className="space-y-4 mb-4">
+      <li className="flex items-center gap-4">
+        <img
+          src={movie_cart.image}
+          alt={movie_cart.name}
+          className="size-16 rounded object-cover"
+        />
+        <div>
+          <h3 className="text-md text-gray-900 font-serif font-bold">
             {movie_cart.name}
-          </h2>
+          </h3>
         </div>
-
-        <div className="img box flex justify-start mt-4">
-          <img
-            src={movie_cart.image}
-            alt={movie_cart.name}
-            className="max-w-40 max-h-48 rounded-lg border-2 border-black"
-          />
+        <div className="flex flex-1 items-center justify-end gap-2">
+          <h4 className="text-xl font-serif text-slate-700">
+            ${movie_cart.price}
+          </h4>
+          <button
+            className="btn btn-primary hover:animate-pulse"
+            onClick={handleDelete}
+          >
+            Remove From Cart
+          </button>
         </div>
-
-        <div className="detail w-full lg:pl-3">
-          <div className="flex items-center justify-end w-full mb-4 mr-4">
-            <h3 className="font-serif font-bold text-3xl leading-9 text-gray-900 mr-10">
-              ${movie_cart.price}
-            </h3>
-            <Zoom delay={100}>
-              <button
-                className="btn btn-primary hover:animate-pulse"
-                onClick={handleDelete}
-              >
-                Remove From Cart
-              </button>
-            </Zoom>
-          </div>
-        </div>
-      </Zoom>
-    </div>
+      </li>
+    </ul>
   );
 }
 
 export default Cart;
+
+// <div className="rounded-3xl border-2 border-slate-700 p-4 lg:p-8 flex-nowrap mb-4 max-lg:max-w-lg max-lg:mx-auto gap-y-4 glass ">
+//   <Zoom>
+//     <div className="flex items-center justify-center w-full mt-2">
+//       <h2 className="italic font-bold font-serif text-4xl leading-9 text-slate-700 mx-4">
+//         {movie_cart.name}
+//       </h2>
+//     </div>
+
+//     <div className="img box flex justify-start mt-4">
+//       <img
+//         src={movie_cart.image}
+//         alt={movie_cart.name}
+//         className="max-w-40 max-h-48 rounded-lg border-2 border-black"
+//       />
+//     </div>
+
+//     <div className="detail w-full lg:pl-3">
+//       <div className="flex items-center justify-end w-full mb-4 mr-4">
+//         <h3 className="font-serif font-bold text-3xl leading-9 text-gray-900 mr-10">
+//           ${movie_cart.price}
+//         </h3>
+//         <Zoom delay={100}>
+//           <button
+//             className="btn btn-primary hover:animate-pulse"
+//             onClick={handleDelete}
+//           >
+//             Remove From Cart
+//           </button>
+//         </Zoom>
+//       </div>
+//     </div>
+//   </Zoom>
+// </div>;
--- client/src/components/CartList.js
+++ client/src/components/CartList.js
@@ -33,43 +33,45 @@ function CartList() {
   return (
     <Fade cascade delay={200}>
       <section className="py-24 relative bg-gradient-to-r from-cyan-500 to-blue-500">
-        <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
-          <h2 className="title font-serif font-bold underline text-4xl leading-10 mb-8 text-center text-black">
-            Shopping Cart
-          </h2>
-          {cartComponent.length > 0 ? (
-            <div className="rounded-3xl border-2 border-gray-200 p-4 lg:p-8 flex-nowrap mb-4 max-lg:max-w-lg max-lg:mx-auto gap-y-4 glass ">
-              {cartComponent}
-            </div>
-          ) : (
-            <div className="text-center">
-              <h1 className="text-black text-6xl font-serif">
-                Your cart is Empty
-              </h1>
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col md:flex-row items-center md:items-center justify-between lg:px-6 pb-6 border-b border-gray-200 max-lg:max-w-lg max-lg:mx-auto">
-          <h5 className="text-gray-900 font-serif font-semibold underline text-2xl leading-9 w-full max-md:text-center max-md:mb-4">
-            Subtotal
-          </h5>
-
-          <div className="flex items-center justify-between gap-5 ">
-            <h6 className="font-serif font-bold text-3xl lead-10 text-black">
-              ${roundedTotal}
-            </h6>
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+          <div className="mx-auto max-w-3xl">
+            <header className="text-center">
+              <h2 className="title font-serif font-bold underline text-4xl leading-10 mb-8 text-center text-black">
+                Shopping Cart
+              </h2>
+            </header>
+            {cartComponent.length > 0 ? (
+              <div className="mt-8">{cartComponent}</div>
+            ) : (
+              <div className="text-center">
+                <h1 className="text-black text-6xl font-serif">
+                  Your cart is Empty
+                </h1>
+              </div>
+            )}
           </div>
-        </div>
-        <div className="max-lg:max-w-lg max-lg:mx-auto">
-          <p className="font-normal text-base leading-7 text-gray-500 text-center mb-5 mt-6">
-            Shipping taxes, and discounts calculated at checkout
-          </p>
-          <button
-            className="rounded-full py-4 px-6 bg-indigo-600 text-white font-semibold text-lg w-full text-center transition-all duration-500 hover:bg-indigo-700 "
-            onClick={toCheckout}
-          >
-            Checkout
-          </button>
+          <div className="flex flex-col md:flex-row items-center md:items-center justify-between lg:px-6 pb-6 border-b border-gray-200 max-lg:max-w-lg max-lg:mx-auto">
+            <h5 className="text-gray-900 font-serif font-semibold underline text-2xl leading-9 w-full max-md:text-center max-md:mb-4">
+              Subtotal
+            </h5>
+
+            <div className="flex items-center justify-between gap-5 ">
+              <h6 className="font-serif font-bold text-3xl lead-10 text-black">
+                ${roundedTotal}
+              </h6>
+            </div>
+          </div>
+          <div className="max-lg:max-w-lg max-lg:mx-auto">
+            <p className="font-normal text-base leading-7 text-gray-500 text-center mb-5 mt-6">
+              Shipping taxes, and discounts calculated at checkout
+            </p>
+            <button
+              className="rounded-full py-4 px-6 bg-indigo-600 text-white font-semibold text-lg w-full text-center transition-all duration-500 hover:bg-indigo-700 "
+              onClick={toCheckout}
+            >
+              Checkout
+            </button>
+          </div>
         </div>
       </section>
     </Fade>
@@ -77,3 +79,46 @@ function CartList() {
 }
 
 export default CartList;
+
+{
+  /* <Fade cascade delay={200}>
+  <section className="py-24 relative bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
+      <h2 className="title font-serif font-bold underline text-4xl leading-10 mb-8 text-center text-black">
+        Shopping Cart
+      </h2>
+      {cartComponent.length > 0 ? (
+        <div className="rounded-3xl border-2 border-gray-200 p-4 lg:p-8 flex-nowrap mb-4 max-lg:max-w-lg max-lg:mx-auto gap-y-4 glass ">
+          {cartComponent}
+        </div>
+      ) : (
+        <div className="text-center">
+          <h1 className="text-black text-6xl font-serif">Your cart is Empty</h1>
+        </div>
+      )}
+    </div>
+    <div className="flex flex-col md:flex-row items-center md:items-center justify-between lg:px-6 pb-6 border-b border-gray-200 max-lg:max-w-lg max-lg:mx-auto">
+      <h5 className="text-gray-900 font-serif font-semibold underline text-2xl leading-9 w-full max-md:text-center max-md:mb-4">
+        Subtotal
+      </h5>
+
+      <div className="flex items-center justify-between gap-5 ">
+        <h6 className="font-serif font-bold text-3xl lead-10 text-black">
+          ${roundedTotal}
+        </h6>
+      </div>
+    </div>
+    <div className="max-lg:max-w-lg max-lg:mx-auto">
+      <p className="font-normal text-base leading-7 text-gray-500 text-center mb-5 mt-6">
+        Shipping taxes, and discounts calculated at checkout
+      </p>
+      <button
+        className="rounded-full py-4 px-6 bg-indigo-600 text-white font-semibold text-lg w-full text-center transition-all duration-500 hover:bg-indigo-700 "
+        onClick={toCheckout}
+      >
+        Checkout
+      </button>
+    </div>
+  </section>
+</Fade>; */
+}
