415c2e9fb8298056829b18067afa81429f7eff10
David Kerr <kerrky@gmail.com>
2024-05-06 16:02:33 -0400

    Removed commented out code

--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -29,13 +29,3 @@ function ReviewList() {
 }
 
 export default ReviewList;
-
-{
-  /* <Fade cascade delay={200}>
-      <div className="flex flex-wrap justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 mt-10">
-        <div>{reviewComponent}</div>
-      </div>
-    </Fade> */
-}
-
-// relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none font-serif
