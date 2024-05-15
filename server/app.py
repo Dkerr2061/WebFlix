c616d64e5205d6d74df32d7e99b1d985c1511b31
David Kerr <kerrky@gmail.com>
2024-05-15 11:05:02 -0400

    Updated vercel file

--- server/app.py
+++ server/app.py
@@ -7,7 +7,8 @@ from flask import request, make_response, session
 from flask_restful import Resource
 
 # Local imports
-from config import app, db, api, bcrypt
+# from config import app, db, api, bcrypt
+from config import *
 # Add your model imports
 from models import Movie, User, CartItem, Review
 
